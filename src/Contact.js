0a76beb7d5b23f450aaf7fbe3460b3951f7c6f11
Roki-IT <gergorec.rok@gmail.com>
2022-04-13 05:35:31 +0200

    Changes

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 export const Contact = () => {
   const form = useRef();
 
-  const sendEmail = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     emailjs
@@ -24,19 +24,20 @@ export const Contact = () => {
         }
       );
     e.target.reset();
-    alert("Message sent succesfully");
+    alert("message sent");
   };
 
   return (
     <div class="contact-container" id="contact-container">
-      <form ref={form} onSubmit={sendEmail}>
+      <form ref={form} onSubmit={handleSubmit}>
         <h3>GET IN TOUCH</h3>
         <label>Name</label>
-        <input type="text" name="name" placeholder="name" />
+        <input type="text" required name="name" placeholder="name" />
         <label>Email</label>
-        <input type="email" name="user_email" placeholder="email" />
+        <input type="email" required name="email" placeholder="email" />
+
         <label>Message</label>
-        <textarea name="message" rows="4" placeholder="message" />
+        <textarea required name="message" rows="4" placeholder="message" />
         <input type="submit" value="Send" className="btn" />
       </form>
     </div>
